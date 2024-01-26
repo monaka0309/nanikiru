606f991f509ae9c2103a5c706ed2cb0e2c9adafa
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-26 19:09:01 +0900

    Fix: データベースの修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_20_055258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "post_tiles", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tile_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tiles_on_post_id"
+    t.index ["tile_id"], name: "index_post_tiles_on_tile_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -22,6 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_20_055258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tiles", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image_path", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tiles_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +52,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_20_055258) do
     t.string "image"
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "post_tile_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_tile_id"], name: "index_votes_on_post_tile_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "post_tiles", "tiles"
   add_foreign_key "posts", "users"
+  add_foreign_key "votes", "post_tiles"
 end
