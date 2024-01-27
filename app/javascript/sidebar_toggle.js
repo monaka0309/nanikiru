09d7a45614132e24b160598a468139ccb31b8c8f
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-27 19:54:26 +0900

    Fix: コンソールのエラーの修正

--- app/javascript/sidebar_toggle.js
+++ app/javascript/sidebar_toggle.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var sidebar = document.getElementById('sidebar');
-    var sidebarToggle = document.getElementById('sidebarToggle');
-
-    sidebarToggle.addEventListener('click', function() {
-        sidebar.classList.toggle('-translate-x-full');
-    });
-});
-
+    var sidebar = document.getElementById("sidebar");
+    var sidebarToggle = document.getElementById("sidebarToggle");
+  
+    if (sidebar && sidebarToggle) {
+      sidebarToggle.addEventListener("click", function() {
+        sidebar.classList.toggle("-translate-x-full");
+      });
+    }
+  });
+  
