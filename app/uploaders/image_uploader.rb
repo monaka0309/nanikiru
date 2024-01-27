45c80d56e54b910191253d5c2a7ec3ef1e05245e
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-27 16:00:51 +0900

    Add: 投稿一覧、投稿詳細画面を少々修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,6 +33,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
 
+  # アップロードを許可するファイルの種類を指定します
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_allowlist
