62ba3f73f45e1f5c38d53dec8f6ed99740a02f41
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-31 11:21:20 +0900

    Fix: heroku上でマイグレーションファイルが作成されたままであったため、再度ダミーを作成し、エラーの解決を図る

---
+++ db/migrate/20240124000720_damy3.rb
@@ -0,0 +1,10 @@
+class Damy3< ActiveRecord::Migration[7.0]
+    def up
+      # ダミーのマイグレーションなので中身は空
+    end
+  
+    def down
+      # ダミーのマイグレーションなので中身は空
+    end
+  end
+  
---
+++ db/migrate/20240124000721_damy4.rb
@@ -0,0 +1,10 @@
+class Damy4 < ActiveRecord::Migration[7.0]
+    def up
+      # ダミーのマイグレーションなので中身は空
+    end
+  
+    def down
+      # ダミーのマイグレーションなので中身は空
+    end
+  end
+  
