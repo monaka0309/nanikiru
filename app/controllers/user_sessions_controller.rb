515449e974ba9d326cfa2407305a92fc73850c0d
naka ryunosuke <naka.sava0309@gmail.com>
2024-02-01 08:47:04 +0900

    Add: ログイン時のエラーメッセージの追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,8 +4,8 @@ class UserSessionsController < ApplicationController
     def new
     end
 
+
     def create
-      # ログイン処理
       user = login(params[:email], params[:password])
       if user
         redirect_back_or_to(root_path, notice: 'ログイン成功しました。')
