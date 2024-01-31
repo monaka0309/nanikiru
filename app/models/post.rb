3ef359615ebd2c3146c1ce3eba995624c3a6f975
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-31 10:15:57 +0900

    Add: 牌を投稿詳細ページに表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  # JSONBカラムのアクセッサを設定
+  store_accessor :selected_images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_19_113025) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_30_082046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_19_113025) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "selected_images", default: []
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
