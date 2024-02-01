de9d35eb2d2f1d1b5562593c724d0d4fe41a3540
naka ryunosuke <naka.sava0309@gmail.com>
2024-02-01 17:27:22 +0900

    error: 新規投稿で牌画像が保存できなくなった

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
   # JSONBカラムのアクセッサを設定
-  store_accessor :selected_images
+  # store_accessor :selected_images
 end
