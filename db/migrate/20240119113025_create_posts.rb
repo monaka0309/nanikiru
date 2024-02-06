6821d669bc14049bd2393078c9012ee06f1e0c80
naka ryunosuke <naka.sava0309@gmail.com>
2024-02-06 17:38:04 +0900

    Fix: rubocop

--- db/migrate/20240119113025_create_posts.rb
+++ db/migrate/20240119113025_create_posts.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# このマイグレーションは、データベース内に 'posts' テーブルを作成します。
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
--- db/migrate/20240130082046_add_selected_images_to_posts.rb
+++ db/migrate/20240130082046_add_selected_images_to_posts.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# このマイグレーションは、'posts' テーブルに 'selected_images' 列を追加します。
 class AddSelectedImagesToPosts < ActiveRecord::Migration[7.0]
   def change
     add_column :posts, :selected_images, :jsonb, default: []
