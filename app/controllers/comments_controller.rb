e582921367789e540adefaec31538e10f53f29cf
naka ryunosuke <naka.sava0309@gmail.com>
2024-03-15 21:06:19 +0900

    Add: コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,8 @@ class CommentsController < ApplicationController
     end
 
     def destroy
+        Comment.find(params[:id]).destroy
+        redirect_to post_path(params[:post_id])
     end
 
     private
