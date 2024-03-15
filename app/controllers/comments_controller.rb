a4e76ee5a3241c62720dd04e5222562b53bd9f96
naka ryunosuke <naka.sava0309@gmail.com>
2024-03-15 21:30:03 +0900

    Add: ログイン時のみコメント可能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,18 @@
 # コメントについてのコントローラー
 class CommentsController < ApplicationController
+    before_action :require_login
+
     def create
-        post = Post.find(params[:post_id])
-        comment = current_user.comments.new(comment_params)
-        comment.post_id = post.id
-        comment.save
-        redirect_to post_path(post)
+        # if user_signed_in?
+            post = Post.find(params[:post_id])
+            comment = current_user.comments.new(comment_params)
+            comment.post_id = post.id
+            comment.save
+            redirect_to post_path(post)
+        # else
+        #     flash[:notice]="ログインが必要です"
+        #     redirect_to new_user_session_path
+        # end
     end
 
     def destroy
