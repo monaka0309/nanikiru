667b741fd824059726734169523f681924236c00
naka ryunosuke <naka.sava0309@gmail.com>
2024-03-16 09:11:16 +0900

    Fix: 日本時間への変更

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Nanikiru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
