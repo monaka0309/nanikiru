d6a3b4f7f24ecd99129922c8c16e9b52d9a71233
naka ryunosuke <naka.sava0309@gmail.com>
2024-03-18 16:42:19 +0900

    Add: 投稿、コメントにユーザー名、アイコン画像を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   authenticates_with_sorcery!
   validates :name, presence: true
-  # validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
