15ba77b5340cdd1c48f9c6e79cd452b27d2b95cd
naka ryunosuke <naka.sava0309@gmail.com>
2024-03-21 16:38:42 +0900

    Fix: routesの修正

--- app/javascript/image_preview.js
+++ app/javascript/image_preview.js
@@ -14,23 +14,23 @@ document.addEventListener("turbo:load", () => {
       });
     }
   });
-  
 
-    document.getElementById('image-input').addEventListener('change', (event) => {
-        const file = event.target.files[0];
-        const imagePreview = document.getElementById('image-preview');
 
-        if (file) {
-            const reader = new FileReader();
+  document.getElementById('image-input').addEventListener('change', (event) => {
+      const file = event.target.files[0];
+      const imagePreview = document.getElementById('image-preview');
 
-            reader.onload = (e) => {
-                imagePreview.innerHTML = `<img src="${e.target.result}" class="max-w-full h-auto" />`;
-            };
+      if (file) {
+          const reader = new FileReader();
 
-            reader.readAsDataURL(file);
-        }
-    });
-});
+          reader.onload = (e) => {
+              imagePreview.innerHTML = `<img src="${e.target.result}" class="max-w-full h-auto" />`;
+          };
+
+          reader.readAsDataURL(file);
+      }
+  });
+// });
   //   const input = document.getElementById("image-input");
   //   if (input) {
   //     input.addEventListener("change", (event) => {
